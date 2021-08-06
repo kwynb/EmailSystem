83da307ccddebfa5ab04a952e6abb28678304191
Kwynett Bragado <kwynett.bragado@ibm.com>
2021-08-06 21:04:22 +0800

    EmailSystem

---
+++ README.md
@@ -0,0 +1,3 @@
+# Email System 
+<hr>
+
---
+++ src/main/java/com/bragado/EmailSystem/EmailSystemApplication.java
@@ -0,0 +1,13 @@
+package com.bragado.EmailSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailSystemApplication.class, args);
+	}
+
+}
